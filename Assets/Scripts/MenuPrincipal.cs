f1f57ef38d8fb7d53f4e63bc4322272223fff609
RiKeGF <rikefinger@hotmail.com>
2019-08-09 16:34:36 -0300

    Primeiro commit

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+
+   public AudioSource[] audio = new AudioSource[2];
+
+
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+
+
+   private void Update()
+   {
+   }
+
+   private void FixedUpdate()
+   {
+      
+
+   }
+
+
+   public void iniciarJogo()
+   {
+      SceneManager.LoadScene("Jogo");
+   }
+
+   public void fecharJogo()
+   {
+      Application.Quit();
+   }
+
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+    public bool pause;
+    public GameObject pauseUI;
+
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        pause = false;
+        pauseUI.SetActive(false);
+    }
+
+    public void PauseOn()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0;
+        pause = true;
+    }
+
+    public void PauseOff()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1;
+        pause = false;
+    }
+
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (!pause)
+            {
+                PauseOn();
+            }
+            else
+            {
+                PauseOff();
+            }
+        }
+
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+
+}
